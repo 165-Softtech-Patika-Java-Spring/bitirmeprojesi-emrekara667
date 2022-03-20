97ae65bca68d518a599ff1471a275549f155f7f0
Emre Kara <emre_kara_97@hotmail.com>
2022-03-20 22:43:05 +0300

    product price update added

---
+++ src/main/java/com/emrekara/finalproject/app/product/dto/PrProductUpdatePriceDto.java
@@ -0,0 +1,12 @@
+package com.emrekara.finalproject.app.product.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PrProductUpdatePriceDto {
+
+    private Long id;
+    private BigDecimal vatFreePrice;
+}
